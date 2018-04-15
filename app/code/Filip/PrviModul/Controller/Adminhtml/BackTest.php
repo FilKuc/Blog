e78644197e52f82e4da7e8dce6e258826886996f
filip <filip.kuc94@gmail.com>
2018-04-15 19:45:52 +0200

    drugi push

---
+++ app/code/Filip/PrviModul/Controller/Adminhtml/BackTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 15.04.18.
+ * Time: 19:13
+ */
+
+namespace Filip\PrviModul\Controller\Adminhtml;
+
+
+class BackTest extends \Magento\Framework\App\Action\Action
+{
+
+    public function execute()
+    {
+        echo 'Hello World';
+    }
+
+
+}
---
+++ app/code/Filip/PrviModul/Controller/FrontTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 15.04.18.
+ * Time: 19:01
+ */
+
+namespace Filip\PrviModul\Controller;
+
+
+class FrontTest extends \Magento\Framework\App\Action\Action
+
+{
+
+    public function execute()
+    {
+       echo 'Hello World';
+    }
+
+
+}
